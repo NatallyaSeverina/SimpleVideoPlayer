25a4eeeb14d7707520e8a97da8acda5ad1929ac2
Natasha <natasha.awesomeyo@gmail.com>
2018-01-20 16:26:14 +0300

    add demo

--- src/js/app.js
+++
@@ -1,2 +0,0 @@
-﻿import SimpleVideoPlayer from "./SimpleVideoPlayer";
-new SimpleVideoPlayer(document.getElementById("videoContainer"), "https://s3-eu-west-1.amazonaws.com/onrewind-test-bucket/big_buck_bunny.mp4");
